0829f23d6fe0c60d227e47fe2916e79c737ebb1d
Snow0420 <zero84yuki1309@gmail.com>
2021-04-24 15:56:35 +0900

    とりあえず色々修正

---
+++ New Unity Project/Assets/Script/CoupleCount.cs
@@ -0,0 +1,34 @@
+﻿//ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+//　カップルカウンター
+//　制作者：なかむらの方のももか
+//ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoupleCount : MonoBehaviour
+{
+    //インスペクタで設定できるやーつ
+   [SerializeField] Text coupleText;
+    //インスペクタでセット出来ないやーつ
+    private int coupleCount;        //カップル数
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coupleCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        coupleText.text= coupleCount.ToString();
+    }
+
+    //カップルを足す関数（プレイヤーが２人射貫いた時に呼び出してね！）
+    public void CouplePlus()
+    {
+       coupleCount +=1;
+	}
+}
